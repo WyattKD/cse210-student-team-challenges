37bb2b755b2272fa9c3eeaacc2d4a6d5b3c41104
WyattKD <wykd47@gmail.com>
2021-10-13 10:52:54 -0600

    get_input

---
+++ 05-jumper/jumper/game/director.py
@@ -0,0 +1,73 @@
+# from game. import
+
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to control the sequence of play.
+    
+    Stereotype:
+        Controller
+
+    Attributes:
+        console (Console): An instance of the class of objects known as Console.
+        keep_playing (boolean): Whether or not the game can continue.
+        seeker (Seeker): An instance of the class of objects known as Seeker.
+        hider (Hider): An instance of the class of objects known as Hider.
+    """
+
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self.console = Console()
+        self.jumper = Jumper()
+        self.interact = Interact()
+        self.word_bank = WordBank()
+        self.keep_playing = True
+        
+    def start_game(self):
+        """Starts the game loop to control the sequence of play.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.keep_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+        """Gets the inputs at the beginning of each round of play. In this case,
+        that means moving the seeker to a new location.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        word = self.word_bank.get_word()
+        blank_word = self.interact.convert_to_blanks(word)
+        self.console.write(blank_word)
+        jumper_display = self.jumper.display_jumper()
+        self.console.write(jumper_display)
+        self.interact.get_guess()
+        
+
+    def do_updates(self):
+        """Updates the important game information for each round of play. In 
+        this case, that means the hider watches the seeker.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        
+        
+
+    def do_outputs(self):
+        """Outputs the important game information for each round of play. In 
+        this case, that means the hider provides a hint.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+
+        
