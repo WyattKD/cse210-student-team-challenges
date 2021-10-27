12c1e152eaea92cba5969bd8a4d97b834dc1a15f
WyattKD <wykd47@gmail.com>
2021-10-27 11:17:32 -0600

    defined classes

---
+++ 07-speed/speed/game/word.py
@@ -0,0 +1,23 @@
+import random
+from game import constants
+from game.actor import Actor
+from game.point import Point
+
+class Food(Actor):
+
+    def __init__(self):
+        self.reset()
+        self._width = constants.DEFAULT_SQUARE_LENGTH
+        self._height = constants.DEFAULT_SQUARE_LENGTH
+    
+    def get_points(self):
+        return self._points
+
+    def reset(self):
+        self._points = random.randint(1, 9)
+        x = random.randint(1, 599)
+        y = random.randint(1, 399)
+        position = Point(x, y)
+        self.set_position(position)
+        self.set_text(f"{self._points}")
+
