34f6e0aeb48b60f6ce7f34e28646129f73d1bf49
Ethan <91624529+ethancmeeker@users.noreply.github.com>
2021-11-01 10:49:24 -0600

    my ignore file

--- 07-speed/speed/__main__.py
+++ 07-speed/speed/__main__.py
@@ -1,7 +1,8 @@
+import os
+os.environ['RAYLIB_BIN_PATH'] = r'C:\Users\ethan\OneDrive\Desktop\cse210-student-solo-checkpoints\07-snake\raylib-2.0.0-Win64-mingw\lib'
 from game.director import Director
 from game.input_service import InputService
 from game.output_service import OutputService
-from asciimatics.screen import Screen 
 
 def main(screen):
     input_service = InputService(screen)
