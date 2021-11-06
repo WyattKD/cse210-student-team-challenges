855e680f1df57f5bbb9a1516ddaa8f7ad9e8fdc6
courtneymickelsen <lam15020@byui.edu>
2021-11-05 19:09:09 -0600

    added functions to calculate, add, subtract points

--- 07-speed/speed/game/score_board.py
+++ 07-speed/speed/game/score_board.py
@@ -1,4 +1,4 @@
-import random
+
 from game.actor import Actor
 from game.point import Point
 
@@ -9,8 +9,9 @@ class ScoreBoard(Actor):
         Information Holder
 
     Attributes: 
-        _points (integer): The number of points the food is worth.
+        _score (integer): The number of points the food is worth.
     """
+
     def __init__(self):
         """The class constructor. Invokes the superclass constructor, initializes points to zero, sets the position and updates the text.
         
@@ -18,17 +19,44 @@ class ScoreBoard(Actor):
             self (Score): an instance of Score.
         """
         super().__init__()
-        self._points = 0
+        self._score = 0
         position = Point(1, 0)
         self.set_position(position)
-        self.set_text(f"Score: {self._points}")
-    
-    def add_points(self, points):
+        self.set_text(f"Score: {self._score}")
+
+    def calculate_points(word):
+        """Calculates the amount of points for the guven word.
+        
+        Args:
+            word (string): The word that needs to be assigned a number of points.
+        """
+        points = 0
+
+        for i in range(len(word)):
+            points += 1
+
+        return points
+
+    def add_points(self, word):
         """Adds the given points to the running total and updates the text.
         
         Args:
             self (Score): An instance of Score.
             points (integer): The points to add.
         """
-        self._points += points
-        self.set_text(f"Score: {self._points}")
+
+        points = self.calculate_points(word)
+        self._score += points
+        self.set_text(f"Score: {self._score}")
+
+    def subtract_points(self, word):
+        """Subtracts the given points from the running total and updates the text.
+        
+        Args:
+            self (Score): An instance of Score.
+            points (integer): The points to add.
+        """
+
+        points = self.calculate_points(word)
+        self._score -= points
+        self.set_text(f"Score: {self._score}")
